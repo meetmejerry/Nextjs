97b5448658a6160ad444a9245f11fbd98480d671
Joel Mathew Philip <jmathewphilip@deloitte.com>
2022-11-30 12:26:09 +0530

    Change home page to use getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -44,34 +44,9 @@ const HomePage = (props) => {
   );
 };
 
-// export async function getServerSideProps(context) {
-//   const req = context.req;
-//   const res = context.res;
-//   const client = await MongoClient.connect(
-//     "mongodb+srv://reactProject:1UGcFnoKzaKsjtSl@cluster0.sc2jwga.mongodb.net/?retryWrites=true&w=majority"
-//   );
-//   const db = client.db();
-//   const meetUpCollection = db.collection("meetups");
-
-//   const data = await meetUpCollection.find().toArray();
-//   console.log(data);
-
-//   client.close();
-//   return {
-//     props: {
-//       meetups: data.map((meetup) => ({
-//         title: meetup.title,
-//         address: meetup.address,
-//         image: meetup.image,
-//         id: meetup._id.toString(),
-//       })),
-//     },
-//   };
-// }
-
-//we dont have to worry about these credentials being appearing in the console log as the below function will be executed only in the
-//server side
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
   const client = await MongoClient.connect(
     "mongodb+srv://reactProject:1UGcFnoKzaKsjtSl@cluster0.sc2jwga.mongodb.net/?retryWrites=true&w=majority"
   );
@@ -91,8 +66,33 @@ export async function getStaticProps() {
         id: meetup._id.toString(),
       })),
     },
-    revalidate: 1,
   };
 }
+
+//we dont have to worry about these credentials being appearing in the console log as the below function will be executed only in the
+//server side
+// export async function getStaticProps() {
+//   const client = await MongoClient.connect(
+//     "mongodb+srv://reactProject:1UGcFnoKzaKsjtSl@cluster0.sc2jwga.mongodb.net/?retryWrites=true&w=majority"
+//   );
+//   const db = client.db();
+//   const meetUpCollection = db.collection("meetups");
+
+//   const data = await meetUpCollection.find().toArray();
+//   console.log(data);
+
+//   client.close();
+//   return {
+//     props: {
+//       meetups: data.map((meetup) => ({
+//         title: meetup.title,
+//         address: meetup.address,
+//         image: meetup.image,
+//         id: meetup._id.toString(),
+//       })),
+//     },
+//     revalidate: 1,
+//   };
+// }
 
 export default HomePage;
